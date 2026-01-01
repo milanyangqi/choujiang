2586d51890db18aee10c5513f07fc201a782357c
milanyangqi <437991663@qq.com>
2026-01-01 10:03:33 +0800

    Update footer: remove ICP info, social links and update copyright

--- src/views/Config/index.vue
+++ src/views/Config/index.vue
@@ -76,22 +76,9 @@ function skip(path: string) {
     <nav>
       <a class="cursor-pointer link link-hover text-inherit" target="_blank" href="https://1kw20.fun">{{ t('footer.thiefEasy') }}</a>
     </nav>
-    <nav>
-      <div class="grid grid-flow-col gap-4">
-        <a href="https://github.com/LOG1997/log-lottery" target="_blank" class="cursor-pointer text-inherit">
-          <svg-icon name="github" />
-        </a>
-        <a href="https://twitter.com/TaborSwift" target="_blank" class="cursor-pointer "><svg-icon name="twitter" /></a>
-        <a href="https://www.instagram.com/log.z1997/" target="_blank" class="cursor-pointer ">
-          <svg-icon name="instagram" />
-        </a>
-      </div>
-    </nav>
+
     <aside>
-      <a class="p-0 m-0 hover:text-primary" href="https://beian.miit.gov.cn/" target="_blank">
-        蜀ICP备2021028666号
-      </a>
-      <p>Copyright © {{ currentYear }} - All right reserved by <a class="link link-primary" href="https://github.com/LOG1997" target="_blank">log1997</a></p>
+      <p>Copyright © {{ currentYear }} - All right reserved by Qingzhu</p>
     </aside>
   </footer>
 </template>
